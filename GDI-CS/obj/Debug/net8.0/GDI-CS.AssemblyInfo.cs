26a0f107050edf652e32d7714569e18623a04a0f
tylersfoot <tylerdepa8@gmail.com>
2024-06-25 21:49:17 -0400

    idk lol

--- GDI-CS/obj/Debug/net8.0/GDI-CS.AssemblyInfo.cs
+++ GDI-CS/obj/Debug/net8.0/GDI-CS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GDI-CS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75cff830a547fa2a621d8a5424e862fb26454a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1272f74fa2d96458a12ea9a46cb659353c77263")]
 [assembly: System.Reflection.AssemblyProductAttribute("GDI-CS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GDI-CS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- non-euclidean/scripts/World.gd
+++ non-euclidean/scripts/World.gd
@@ -3,6 +3,9 @@ extends Node3D
 # Preload the player scene
 var PlayerScene = preload("res://scenes/Player.tscn")
 
+@onready var portal_camera = $Portal1/SubViewport/Camera3D
+
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	SimpleGrass.set_interactive(true)
@@ -17,6 +20,13 @@ func _ready():
 	get_window().transparent_bg = false
 	get_tree().get_root().set_transparent_background(false)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, false, 0)
-	
+
+	# camera colors
+	$Portal1/SubViewport/Camera3D.get_node("MeshInstance3D").get_mesh().surface_set_material(0, $PortalFrame1/FrameTop/MeshInstance3D.get_mesh().surface_get_material(0))
+	$Portal2/SubViewport/Camera3D.get_node("MeshInstance3D").set_mesh($Portal2/SubViewport/Camera3D.get_node("MeshInstance3D").get_mesh().duplicate())
+	$Portal2/SubViewport/Camera3D.get_node("MeshInstance3D").get_mesh().surface_set_material(0, $PortalFrame2/FrameTop/MeshInstance3D.get_mesh().surface_get_material(0))
 func _process(_delta):
+	# print("camblue position: ", $Portal1/SubViewport/Camera3D.global_transform.origin)
+	# print("camorange position: ", $Portal2/SubViewport/Camera3D.global_transform.origin)
+	# print("cam basis: ", portal_camera.global_transform.basis)
 	pass

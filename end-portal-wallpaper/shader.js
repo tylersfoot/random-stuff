49816ea19324e28a58d289351024b207fbebc187
tylersfoot <tylerdepa8@gmail.com>
2024-09-10 20:59:35 -0400

    idk

--- end-portal-wallpaper/shader.js
+++ end-portal-wallpaper/shader.js
@@ -3,8 +3,8 @@ attribute vec4 aPosition;
 varying vec4 vPosition;
 
 void main() {
-  gl_Position = aPosition;
   vPosition = aPosition;
+  gl_Position = vPosition;
 }
 `;
 const fragmentCode = `
@@ -13,10 +13,12 @@ precision mediump float;
 uniform float animationTime;
 uniform vec2 parallax; // x and y movement
 uniform vec3 colors[16];
+uniform vec2 canvasSize;
+uniform vec2 screenSize;
+uniform float scale; // 1.0 is normal
 
 varying vec4 vPosition;
 
-const int EndPortalLayers = 15;
 const float PI2 = 6.283185307179586476925286766559;
 
 uniform sampler2D endTexture;
@@ -38,7 +40,15 @@ vec4 end_portal_layer(float layer, vec4 proj) {
     0.0, 0.0, 0.0, 1.0
   );
 
-  proj.xy *= 0.5; // zooms in, basically
+  // scale and resize to prevent stretching
+  proj.xy *= canvasSize.xy * (0.00070 / scale);
+
+  // resize based on screen size (roughly), so that 4k and 1080p look the same size
+  if (screenSize.x / screenSize.y > 1.0) {
+    proj.xy *= 1080.0 / screenSize.x;
+  } else {
+    proj.xy *= 1920.0 / screenSize.y;
+  }
 
   // x and y translation for parallax (mouse or scrolling)
   // 759375000.0 = (15 ^ 5) / 0.001
@@ -60,17 +70,21 @@ vec4 end_portal_layer(float layer, vec4 proj) {
 
 void main() {
   vec3 color = vec3(0.0);
-  for (int i = 0; i < EndPortalLayers; i++) {
-    color += texture2DProj(endTexture, end_portal_layer(float(i + 1), vPosition)).rgb * colors[int(mod(float(i), 16.0))];
+  for (int i = 0; i < 15; i++) {
+    color += texture2DProj(endTexture, end_portal_layer(float(i+1), vPosition)).rgb * colors[int(mod(float(i), 16.0))];
   }
-
   gl_FragColor = vec4(color, 1.0);
 }
 `;
 
 var gl;
+var shaderCanvas;
+var shaderDiv;
 var shaderParallaxUniformLocation;
 var shaderAnimationTimeUniformLocation;
+var shaderCanvasSizeUniformLocation;
+var shaderScreenSizeUniformLocation;
+var shaderScaleUniformLocation;
 
 var shaderParallaxCurrent = [0, 0];
 var shaderParallaxTarget = [0, 0];
@@ -78,12 +92,14 @@ var shaderLerpSpeed = 0.02;
 var shaderLastTime = 0;
 var shaderFrame = 0;
 var shaderTime = 0;
+var shaderScale = 1;
 
 // wallpaper engine properties
 var fps = 60; // 1 - 240
 var mouseReactive = true; // true or false
 var mouseSpeed = 3; // 0 - 10
 var idleSpeed = 1; // 0 - 10
+var shaderScale = 1; // 0.1 - 10
 
 window.wallpaperPropertyListener = {
   applyUserProperties: function(properties) {
@@ -100,10 +116,13 @@ window.wallpaperPropertyListener = {
     if (properties.idlespeed) {
       idleSpeed = properties.idlespeed.value;
     }
+    if (properties.scale) {
+      shaderScale = properties.scale.value;
+    }
   },
 };
 
-function lerp2(start, end, amt) {
+function lerp(start, end, amt) {
   return (1-amt)*start+amt*end
 }
 
@@ -141,8 +160,9 @@ function createProgram(gl, vertexShader, fragmentShader) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  var canvas = document.querySelector('#end-portal-canvas');
-  gl = canvas.getContext('webgl');
+  shaderCanvas = document.querySelector('#end-portal-canvas');
+  shaderDiv = document.querySelector('#end-portal');
+  gl = shaderCanvas.getContext('webgl');
 
   if (gl) {
     // load shaders from html and create the shaders
@@ -174,12 +194,12 @@ document.addEventListener('DOMContentLoaded', () => {
       gl.bindTexture(gl.TEXTURE_2D, texture);
       gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, shaderEndTexture);
       
-      // Set texture parameters
+      // set texture parameters
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    
+
       var shaderEndTextureLocation = gl.getUniformLocation(shaderProgram, "endTexture");
       gl.uniform1i(shaderEndTextureLocation, 0);
 
@@ -204,6 +224,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
       shaderParallaxUniformLocation = gl.getUniformLocation(shaderProgram, "parallax");
       shaderAnimationTimeUniformLocation = gl.getUniformLocation(shaderProgram, "animationTime");
+      shaderCanvasSizeUniformLocation = gl.getUniformLocation(shaderProgram, "canvasSize");
+      shaderScreenSizeUniformLocation = gl.getUniformLocation(shaderProgram, "screenSize");
+      shaderScaleUniformLocation = gl.getUniformLocation(shaderProgram, "scale");
       var shaderColorsUniformLocation = gl.getUniformLocation(shaderProgram, "colors");
 
       const shaderColors = [0.022087, 0.098399, 0.110818,
@@ -223,8 +246,6 @@ document.addEventListener('DOMContentLoaded', () => {
         0.204675, 0.390010, 0.302066,
         0.080955, 0.314821, 0.661491]
       gl.uniform3fv(shaderColorsUniformLocation, shaderColors);
-
-      gl.drawArrays(gl.TRIANGLES, 0, 6);
   
       // start rendering loop
       requestAnimationFrame(render);
@@ -236,48 +257,32 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-// function render() {
-//   var shaderCurrentTime = performance.now();
-//   var shaderTimeSinceLastFrame = shaderCurrentTime - shaderLastTime;
-
-//   if (mouseReactive) {
-//     var shaderParallaxAmount = shaderLerpSpeed * (shaderTimeSinceLastFrame);
-//     shaderParallaxAmount /= shaderParallaxAmount + 5;
-
-//     shaderParallaxCurrent[0] = lerp2(shaderParallaxCurrent[0], shaderParallaxTarget[0], shaderParallaxAmount);
-//     shaderParallaxCurrent[1] = lerp2(shaderParallaxCurrent[1], shaderParallaxTarget[1], shaderParallaxAmount);
-//     gl.uniform2f(shaderParallaxUniformLocation, (shaderParallaxCurrent[0] * mouseSpeed) / 10, (-shaderParallaxCurrent[1] * mouseSpeed) / 10); // vec2 output
-//   }
-
-//   gl.uniform1f(shaderAnimationTimeUniformLocation, ((shaderCurrentTime * idleSpeed) % 1200000) / 1200000);
-
-//   gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-//   while (shaderCurrentTime - shaderLastTime < 1000 / fps) {
-//     shaderCurrentTime = performance.now();
-//   }
-
-//   shaderLastTime = shaderCurrentTime;
-//   // request the next frame
-//   requestAnimationFrame(render);
-// }
-
-function render(currentTime) {
-  var shaderTimeSinceLastFrame = currentTime - shaderLastTime;
+function render(shaderCurrentTime) {
+  var shaderTimeSinceLastFrame = shaderCurrentTime - shaderLastTime;
 
   if (shaderTimeSinceLastFrame > (1000 / fps)) {
-    shaderLastTime = currentTime - (shaderTimeSinceLastFrame % (1000 / fps));
+    shaderLastTime = shaderCurrentTime - (shaderTimeSinceLastFrame % (1000 / fps));
 
     if (mouseReactive) {
       var shaderParallaxAmount = shaderLerpSpeed * shaderTimeSinceLastFrame;
       shaderParallaxAmount /= shaderParallaxAmount + 5;
-
-      shaderParallaxCurrent[0] = lerp2(shaderParallaxCurrent[0], shaderParallaxTarget[0], shaderParallaxAmount);
-      shaderParallaxCurrent[1] = lerp2(shaderParallaxCurrent[1], shaderParallaxTarget[1], shaderParallaxAmount);
+      shaderParallaxCurrent[0] = lerp(shaderParallaxCurrent[0], shaderParallaxTarget[0], shaderParallaxAmount);
+      shaderParallaxCurrent[1] = lerp(shaderParallaxCurrent[1], shaderParallaxTarget[1], shaderParallaxAmount);
       gl.uniform2f(shaderParallaxUniformLocation, (shaderParallaxCurrent[0] * mouseSpeed) / 10, (-shaderParallaxCurrent[1] * mouseSpeed) / 10); // vec2 output
     }
 
-    gl.uniform1f(shaderAnimationTimeUniformLocation, ((currentTime * idleSpeed) % 1200000) / 1200000);
+    // use Date.now() for global sync
+    gl.uniform1f(shaderAnimationTimeUniformLocation, ((Date.now() * idleSpeed) % 1200000) / 1200000);
+
+    if (shaderDiv.clientWidth !== gl.canvas.width || shaderDiv.clientHeight !== gl.canvas.height) {
+      gl.canvas.width = shaderDiv.clientWidth;
+      gl.canvas.height = shaderDiv.clientHeight;
+      gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    }
+
+    gl.uniform2f(shaderCanvasSizeUniformLocation, gl.canvas.width, gl.canvas.height);
+    gl.uniform2f(shaderScreenSizeUniformLocation, screen.width * window.devicePixelRatio, screen.height * window.devicePixelRatio);
+    gl.uniform1f(shaderScaleUniformLocation, shaderScale);
 
     gl.drawArrays(gl.TRIANGLES, 0, 6);
   }

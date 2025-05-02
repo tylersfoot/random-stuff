4f18c19941bd619c2a1202b2e86cce03e5ed786a
tylersfoot <tylerdepa8@gmail.com>
2025-05-01 23:52:43 -0400

    frame merge init

--- frame-merge/src/main.rs
+++ frame-merge/src/main.rs
@@ -1,3 +1,52 @@
+
+use std::fs::create_dir_all;
+use video_rs::decode::Decoder;
+use video_rs::Url;
+use image::{ImageBuffer, Rgb};
+
+
 fn main() {
-    println!("Hello, world!");
+    video_rs::init().unwrap();
+
+    let source = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        .parse::<Url>()
+        .unwrap();
+    let mut decoder = Decoder::new(source).expect("failed to create decoder");
+
+    let output_folder = "frames_video_rs";
+    create_dir_all(output_folder).expect("failed to create output directory");
+
+    let (width, height) = decoder.size();
+    let frame_rate = decoder.frame_rate(); // Assuming 30 FPS if not available
+
+    let max_duration = 1.0; // Max duration in seconds
+    let max_frames = (frame_rate * max_duration).ceil() as usize;
+
+    let mut frame_count = 0;
+    let mut elapsed_time = 0.0;
+    // let mut tasks = vec![];
+
+    for frame in decoder.decode_iter() {
+        if let Ok((_timestamp, frame)) = frame {
+            if elapsed_time > max_duration {
+                break;
+            }
+
+            let rgb = frame.slice(ndarray::s![.., .., 0..3]).to_slice().unwrap();
+
+            let img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::from_raw(width, height, rgb.to_vec())
+            .expect("failed to create image buffer");
+
+            let frame_path = format!("{}/frame_{:05}.png", output_folder, frame_count);
+
+            img.save(&frame_path).expect("failed to save frame");
+
+            frame_count += 1;
+            elapsed_time += 1.0 / frame_rate;
+        } else {
+            break;
+        }
+    }
+
+    println!("Saved {} frames in the '{}' directory", frame_count, output_folder);
 }
